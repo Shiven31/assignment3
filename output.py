97648cb9cc2b905aafea20b86085895d5b5f4753
Shiven31 <96771819+Shiven31@users.noreply.github.com>
2022-02-14 20:45:45 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,83 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================== RESTART: E:/python/ass3/as3.py ========================
+Question Number 1
+ Enter input- shiven.ee
+Total occurances are
+s-1
+h-1
+i-1
+v-1
+e-3
+n-1
+.-1
+Question Number 2
+ enter Day- 2
+ enter Month- 22
+ enter Year- 2222
+That's not valid 
+Question Number3
+ enter Y to enter data- y
+ give an integer input- 5
+ enter Y to enter data- y
+ give an integer input- 3
+ enter Y to enter data- y
+ give an integer input- 4
+ enter Y to enter data- y
+ give an integer input- 6
+ enter Y to enter data- n
+[5, 3, 4, 6]
+[(5, 25), (3, 9), (4, 16), (6, 36)]
+Question Number4
+Enter Grade Points- 9
+Your Grade is A and Excellent performance 
+Question Number5
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+Question 6
+ Enter SID of Student= 21104017
+enter the name of the Student= Shiven goyal
+If you want to enter more details press Y/N= Y
+ Enter SID of Student= 21100000
+enter the name of the Student= abcdefu
+If you want to enter more details press Y/N= N
+Part a
+
+21104017 is Shiven goyal
+21100000 is abcdefu
+
+Part b
+value sorted dictionary is ['Shiven goyal', 'abcdefu']
+
+Part c
+Keys sorted dictionary is [21100000, 21104017]
+
+Part D
+Please enter the student's SID whose detail you need- 21104017
+Name of the required student is Shiven goyal
+
+Question Number 7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 5
+Fibonnaci sequence with 5 terms
+0
+1
+1
+2
+3
+Average of total 5 terms of sequence is 1.4
+Question Number8
+Part a
+set with elements not common in both is {1, 3, 5, 6, 8}
+Part b
+Set of elements that is only present in one set is {17, 3, 6, 8, 9, 13}
+Part C
+Set of elements that is only present in two set is {1, 2, 4, 5}
+Part d
+set of all integers in the range 1 to 10 that are not in Set1 {6, 7, 8, 9, 10}
+Part e
+set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3 {10, 7}
